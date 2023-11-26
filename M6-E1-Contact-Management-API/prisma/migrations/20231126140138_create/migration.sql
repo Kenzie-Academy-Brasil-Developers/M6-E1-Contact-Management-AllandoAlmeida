5ace5aedbafc6c63727c6386a92a2ed8356474ee
AllandoAlmeida <alandoalmeida0803@7j50z3.onmicrosoft.com>
2023-11-26 12:09:49 -0300

    fix: correction in the relationship of customer and contact tables

---
+++ M6-E1-Contact-Management-API/prisma/migrations/20231126140138_create/migration.sql
@@ -0,0 +1,79 @@
+-- CreateTable
+CREATE TABLE "customers" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "username" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3),
+    "isActive" BOOLEAN NOT NULL DEFAULT true,
+
+    CONSTRAINT "customers_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "contacts" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "cep" TEXT NOT NULL,
+    "address" TEXT NOT NULL,
+    "complement" TEXT NOT NULL,
+    "neighborhood" TEXT NOT NULL,
+    "locality" TEXT NOT NULL,
+    "uf" TEXT NOT NULL,
+    "gia" INTEGER NOT NULL,
+    "ddd" INTEGER NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated" TIMESTAMP(3),
+    "isActive" BOOLEAN NOT NULL DEFAULT true,
+
+    CONSTRAINT "contacts_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "contactToCustomer" (
+    "id" TEXT NOT NULL,
+    "customerId" TEXT NOT NULL,
+    "contactId" TEXT NOT NULL,
+    "assignedAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "contactToCustomer_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "phones" (
+    "id" TEXT NOT NULL,
+    "telephone" TEXT NOT NULL,
+    "customerId" TEXT,
+    "contactId" TEXT,
+
+    CONSTRAINT "phones_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "emails" (
+    "id" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "customerId" TEXT,
+    "contactId" TEXT,
+
+    CONSTRAINT "emails_pkey" PRIMARY KEY ("id")
+);
+
+-- AddForeignKey
+ALTER TABLE "contactToCustomer" ADD CONSTRAINT "contactToCustomer_customerId_fkey" FOREIGN KEY ("customerId") REFERENCES "customers"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "contactToCustomer" ADD CONSTRAINT "contactToCustomer_contactId_fkey" FOREIGN KEY ("contactId") REFERENCES "contacts"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "phones" ADD CONSTRAINT "phones_customerId_fkey" FOREIGN KEY ("customerId") REFERENCES "customers"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "phones" ADD CONSTRAINT "phones_contactId_fkey" FOREIGN KEY ("contactId") REFERENCES "contacts"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "emails" ADD CONSTRAINT "emails_customerId_fkey" FOREIGN KEY ("customerId") REFERENCES "customers"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "emails" ADD CONSTRAINT "emails_contactId_fkey" FOREIGN KEY ("contactId") REFERENCES "contacts"("id") ON DELETE SET NULL ON UPDATE CASCADE;
